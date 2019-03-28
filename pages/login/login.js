5dd1322d57dc17361bf5b1bd9c97a3ec5bcdb526
fatfan <fatfan1020@gmail.com>
2019-03-28 17:01:42 +0800

    登录页面/一键登录功能

---
+++ pages/login/login.js
@@ -0,0 +1,37 @@
+const { http } = require('../../lib/http.js')
+const { app_id, app_secret } = getApp().globalData
+
+Page({
+  data: {
+
+  },
+  //点击按钮 => 调用小程序原生的 wx.login => 参数 => http.post => 返回 user
+  // => 保存user => 返回首页
+ login(event){
+   let encrypted_data = event.detail.encryptedData
+   let iv = event.detail.iv
+   this.wxLogin(encrypted_data, iv)
+ },
+  wxLogin(encrypted_data, iv){
+    wx.login({
+      success: (res) => this.loginMe(res.code, iv, encrypted_data)
+    })
+  },
+  loginMe(code, iv, encrypted_data){
+    http.post('/sign_in/mini_program_user',{
+      code,
+      iv,
+      encrypted_data,
+      app_id,
+      app_secret
+    })
+    .then((response)=>{
+      this.saveMessage(response)
+      wx.reLaunch({ url: "/pages/home/home" })
+    })
+  },
+  saveMessage(response){
+    wx.setStorageSync('me', response.data.detail )
+    wx.setStorageSync('X-token', response.header["X-token"])
+  }
+})
